0e356891d81d413069c8837b31e5c32723b88be5
Zac Echola <zac.echola@gmail.com>
2012-12-06 09:29:52 -0600

    trivial

--- fantasy.py
+++ fantasy.py
@@ -7,6 +7,9 @@ MATCHUPS = raw_input("How many matchups to run? ")
 if int(MATCHUPS) < 6000:
     print "Run at least 6000 matchups to avoid errors!"
     exit()
+# make bets and win money
+
+spread_baseline = int(raw_input("Enter a spread: "))
 
 me = array(
         [
@@ -18,7 +21,7 @@ me = array(
         [12.25, 2.50],  #pettigrew
         [16.03, 3.14],  #johnson
         [11.15, 3.03],  #49ers
-        [10.03, 3.13],   #hanson
+        [10.03, 3.13],  #hanson
         ]
         )
 
@@ -28,7 +31,7 @@ opp = array(
         [15.88, 2.84],  #johnson
         [16.53, 2.86],  #spiller
         [20.20, 6.16],  #green
-        [12., 4.79],   #bowe
+        [12., 4.79],    #bowe
         [15.70, 6.22],  #witten
         [16., 5.35],    #graham
         [3.55, 1.83],   #ravens
@@ -62,7 +65,7 @@ def score(data):
     for i in data:
         mu = sum(i[:1])
         sigma = sum(i[1:])
-        score = int(normalvariate(mu, sigma))
+        score = int(normalvariate(mu, sigma)) # ESPN league is whole numbers
         points += score
     return points
 
@@ -106,7 +109,7 @@ for i in range(0, int(MATCHUPS)):
     if opp_score == me_score:
         ties += 1
 
-
+#output
 print "\n----"
 print "Win %:"
 print (float(wins) / float(MATCHUPS)) * 100
@@ -115,6 +118,7 @@ print "My average:"
 print float(sum(me_normal_average)) / float(len(me_normal_average))
 print "Opponent average:"
 print float(sum(opp_normal_average)) / float(len(opp_normal_average))
+
 print ""
 print "My highest score:"
 print me_high_score
@@ -125,6 +129,7 @@ print "Opponent highest score:"
 print opp_high_score
 print "Opponent low score:"
 print opp_low_score
+
 print ""
 print "Opponent winning score average:"
 opp_win_score_average = float(sum(opp_win_average)) / float(len(opp_win_average))
@@ -135,15 +140,17 @@ print opp_win_spread
 print "Ties %:"
 print float((ties) / float(MATCHUPS)) * 100
 
+print ""# make bets and win money
+
 print "My winning score average:"
 me_win_score_average = float(sum(me_win_average)) / float(len(me_win_average))
 print me_win_score_average
 print "My average winning spread:"
 me_win_spread = float(sum(me_average_win_spread)) / float(len(me_average_win_spread))
 print me_win_spread
+
 print ""
-baseline = 10
-print "My win percent over %d" % baseline
-print (spread_freq(baseline) / float(MATCHUPS)) * 100
+print "My win spread percent over %d points" % spread_baseline
+print (spread_freq(spread_baseline) / float(MATCHUPS)) * 100
 
 print "----\n"
